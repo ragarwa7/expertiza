f94a16d79b4352e436c261d53e56672124a6b2cb
Rachit Agarwal <ragarwa7@ncsu.edu>
2018-12-11 16:03:59 -0500

    Security_fix: updating response_map.rb, review_grade.rb, and review_response_map.rb models for protecting potentially dangerous attributes.

--- app/models/review_grade.rb
+++ app/models/review_grade.rb
@@ -1,4 +1,5 @@
 class ReviewGrade < ActiveRecord::Base
   belongs_to :participant
-  attr_accessible :participant_id, :grade_for_reviewer, :comment_for_reviewer, :review_graded_at, :reviewer_id
+  attr_accessible  :grade_for_reviewer, :comment_for_reviewer, :review_graded_at
+  attr_protected :participant_id, :reviewer_id
 end
